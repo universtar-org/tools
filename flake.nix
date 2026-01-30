221d0ac7b7b5b1404af436bedcf400896b45236e
Fovir <fovir@disroot.org>
2026-01-30 11:09:27 +0800

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Devshell for Golang.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      # Add packages here.
+      buildInputs = with pkgs; [
+        go
+        gopls
+        gotools
+        govulncheck
+      ];
+
+      # Shell hooks.
+      shellHook = ''
+        echo "Entering the development environment!"
+        go version
+      '';
+    };
+  };
+}
