e4d5dfcf5bec33f2e3eee87b473aaaed339e0813
Fovir <fovir@disroot.org>
2026-02-20 11:36:49 +0800

    test: add testing for `ParseOwner()`

---
+++ internal/utils/parser_test.go
@@ -0,0 +1,42 @@
+package utils_test
+
+import (
+	"github.com/universtar-org/tools/internal/utils"
+	"testing"
+)
+
+func TestParseOwner(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		path string
+		want string
+	}{
+		{
+			name: "NormalOwner",
+			path: "alice.yaml",
+			want: "alice",
+		},
+		{
+			name: "UppercaseOwner",
+			path: "BOB.YAML",
+			want: "BOB",
+		},
+		{
+			name: "OwnerWithDash",
+			path: "foo-bar.yaml",
+			want: "foo-bar",
+		},
+		{
+			name: "OwnerWithDots",
+			path: "foo.foo.bar.bar.yaml",
+			want: "foo.foo.bar.bar",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := utils.ParseOwner(tt.path)
+			utils.HandleTestDiff(t, tt.want, got)
+		})
+	}
+}
